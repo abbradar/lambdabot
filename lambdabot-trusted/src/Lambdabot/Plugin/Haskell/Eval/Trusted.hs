3d25b390d884eb08f78cdd008910c337063581bf
Bertram Felgenhauer <int-e@gmx.de>
2015-04-03 00:11:46 +0200

    import random things from GHC.Exts

--- lambdabot-trusted/src/Lambdabot/Plugin/Haskell/Eval/Trusted.hs
+++ lambdabot-trusted/src/Lambdabot/Plugin/Haskell/Eval/Trusted.hs
@@ -4,11 +4,13 @@ module Lambdabot.Plugin.Haskell.Eval.Trusted
     , module Test.QuickCheck
     , module Lambdabot.Plugin.Haskell.Check.ShowQ
     , module Lambdabot.Plugin.Haskell.Eval.Trusted
+    , module GHC.Exts, Constraint
     ) where
 
 import Math.OEIS
 import Lambdabot.Plugin.Haskell.Check.ShowQ
 import Test.QuickCheck
+import GHC.Exts (Constraint, IsList (..), IsString (..))
 
 describeSequence :: SequenceData -> Maybe String
 describeSequence = fmap description . lookupSequence
